c6fa992d0e311f1986e588794dbae5a886cc2bbf
Semih <77493886+semih5@users.noreply.github.com>
2022-02-03 14:21:33 +0300

    Update

--- mod4_solution/SpeakGoodBy.js
+++ mod4_solution/SpeakGoodBy.js
@@ -1,11 +1,11 @@
 (function(window){
 
-		var speakWord = "Good Bye";
-		var bySpeaker =  {};
+	var speakWord = "Good Bye";
+	var bySpeaker =  {};
 
-		bySpeaker.speak = function (name) {
-    	console.log(speakWord + " " + name);
-		}
-  	window.bySpeaker = bySpeaker;
+	bySpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+	}
+  window.bySpeaker = bySpeaker;
 
 })(window);
